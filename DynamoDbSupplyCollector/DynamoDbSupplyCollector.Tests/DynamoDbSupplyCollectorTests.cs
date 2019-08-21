50b6092f2819ac9e8b46c7b8b24bcf5b8b211342
Hlushyn <EGlushin@kiev.luxoft.com>
2019-08-21 18:05:58 +0300

    Implement TestConnection and GetDataCollectionMetrics.

--- DynamoDbSupplyCollector/DynamoDbSupplyCollector.Tests/DynamoDbSupplyCollectorTests.cs
+++ DynamoDbSupplyCollector/DynamoDbSupplyCollector.Tests/DynamoDbSupplyCollectorTests.cs
@@ -1,5 +1,8 @@
 using FluentAssertions;
+using S2.BlackSwan.SupplyCollector.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace DynamoDbSupplyCollector.Tests
@@ -7,6 +10,8 @@ namespace DynamoDbSupplyCollector.Tests
     public class DynamoDbSupplyCollectorTests
     {
         private DynamoDbSupplyCollector _sut;
+
+        readonly List<string> KNOWN_TABLES = new List<string> { "CONTACTS_AUDIT", "EMAILS", "LEADS", "PEOPLE" };
 
         public DynamoDbSupplyCollectorTests()
         {
@@ -17,6 +22,36 @@ namespace DynamoDbSupplyCollector.Tests
         public void DataStoreTypes_returns_dynamodb()
         {
             _sut.DataStoreTypes().Should().Contain("DynamoDB");
+        }
+
+        [Fact]
+        public void TestConnection()
+        {
+            // arrange
+            var container = new DataContainer
+            {
+                ConnectionString = "ServiceURL=http://localhost:8000; AccessKey=key_id; AccessSecret=access_key"
+            };
+
+            _sut.TestConnection(container).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetDataCollectionMetrics()
+        {
+            // arrange
+            var container = new DataContainer
+            {
+                ConnectionString = "ServiceURL=http://localhost:8000; AccessKey=key_id; AccessSecret=access_key"
+            };
+
+            // act
+            var metrics = _sut.GetDataCollectionMetrics(container);
+
+            // assert
+
+            metrics.Select(m => m.Name).Should().BeEquivalentTo(KNOWN_TABLES);
+            metrics.Should().HaveCount(KNOWN_TABLES.Count);
         }
     }
 }
--- DynamoDbSupplyCollector/DynamoDbSupplyCollector/DynamoDbSupplyCollector.cs
+++ DynamoDbSupplyCollector/DynamoDbSupplyCollector/DynamoDbSupplyCollector.cs
@@ -1,8 +1,11 @@
-﻿using S2.BlackSwan.SupplyCollector;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using S2.BlackSwan.SupplyCollector;
 using S2.BlackSwan.SupplyCollector.Models;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace DynamoDbSupplyCollector
 {
@@ -20,7 +23,28 @@ namespace DynamoDbSupplyCollector
 
         public override List<DataCollectionMetrics> GetDataCollectionMetrics(DataContainer container)
         {
-            throw new NotImplementedException();
+            var client = GetClient(container.ConnectionString);
+
+            var tables = GetTables(client).GetAwaiter().GetResult();
+
+            var metrics = new List<DataCollectionMetrics>();
+            foreach (var table in tables)
+            {
+                var request = new DescribeTableRequest(table);
+
+                var response = client.DescribeTableAsync(request).GetAwaiter().GetResult();
+
+                var tbl = response.Table;
+                metrics.Add(new DataCollectionMetrics
+                {
+                    Name = tbl.TableName,
+                    UsedSpaceKB = tbl.TableSizeBytes / 1000,
+                    TotalSpaceKB = tbl.TableSizeBytes / 1000,
+                    RowCount = tbl.ItemCount,
+                });
+            }
+
+            return metrics;
         }
 
         public override (List<DataCollection>, List<DataEntity>) GetSchema(DataContainer container)
@@ -30,7 +54,76 @@ namespace DynamoDbSupplyCollector
 
         public override bool TestConnection(DataContainer container)
         {
-            throw new NotImplementedException();
+            var client = GetClient(container.ConnectionString);
+
+            var request = new ListTablesRequest
+            {
+                Limit = 100,
+                ExclusiveStartTableName = null
+            };
+
+            try
+            {
+                client.ListTablesAsync(request).GetAwaiter().GetResult();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private AmazonDynamoDBClient GetClient(string connectionString)
+        {
+            var sections = connectionString.Split(';');
+
+            var serviceUrlSection = sections[0];
+
+            var keyIdSection = sections[1];
+
+            var accessKeySection = sections[2];
+
+            var clientConfig = new AmazonDynamoDBConfig() { ServiceURL = Value(serviceUrlSection) };
+            var client = new AmazonDynamoDBClient(Value(keyIdSection), Value(accessKeySection), clientConfig);
+
+            return client;
+        }
+
+        private DynamoDBContext GetContext(string connectionString)
+        {
+            var contextConfig = new DynamoDBContextConfig() { TableNamePrefix = "" };
+            var context = new DynamoDBContext(GetClient(connectionString), contextConfig);
+
+            return context;
+        }
+
+        string Value(string pair)
+        {
+            return pair.Split("=")[1];
+        }
+
+        async Task<List<string>> GetTables(AmazonDynamoDBClient client)
+        {
+            var tableNames = new List<string>();
+            string lastTableNameEvaluated = null;
+
+            do
+            {
+                // needs to be in a loop because ListTablesAsync can return up to 100 records at a time
+                var request = new ListTablesRequest
+                {
+                    Limit = 100,
+                    ExclusiveStartTableName = lastTableNameEvaluated
+                };
+
+                var response = await client.ListTablesAsync(request);
+                foreach (string name in response.TableNames)
+                    tableNames.Add(name);
+
+                lastTableNameEvaluated = response.LastEvaluatedTableName;
+            } while (lastTableNameEvaluated != null);
+
+            return tableNames;
         }
     }
 }
